3e00fe1b60acc2ae6a17077e90777e8aed4364f8
Shane Neff <shane.neff@insight.com>
2020-10-19 11:55:46 -0400

    remove republic specifics

--- SCRIPTS/install-application.ps1
+++ SCRIPTS/install-application.ps1
@@ -4,21 +4,21 @@ $installers = @()
 
 #source files for installations
 $filelist = @()
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/npp.7.9.Installer.exe"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/CrewPlanning20200912/S3RUSCrewPlanningSystemSetup.msi"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/PairingEditor20200726/S3RUSPairingEditorSetup.msi"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/CrewPlanningUpdates/2020_09%20S3RUS%20System%20Update_mt.exe"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/FreeCommanderXE-32-public_810a.msi"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/OneDriveSetup.exe"
-$filelist += "https://sts3rus.blob.core.windows.net/artifacts/TableauReader-64bit-2020-3-1.exe"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/npp.7.9.Installer.exe"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/CrewPlanning20200912/neffwvdCrewPlanningSystemSetup.msi"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/PairingEditor20200726/neffwvdPairingEditorSetup.msi"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/CrewPlanningUpdates/2020_09%20neffwvd%20System%20Update_mt.exe"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/FreeCommanderXE-32-public_810a.msi"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/OneDriveSetup.exe"
+$filelist += "https://stneffwvd.blob.core.windows.net/artifacts/TableauReader-64bit-2020-3-1.exe"
 
 #source files for app setting updates
 $appfiles = @()
-$appfiles += "https://sts3rus.blob.core.windows.net/artifacts/New%20Files/addparms.txt"
-$appfiles += "https://sts3rus.blob.core.windows.net/artifacts/New%20Files/PairingCategories.txt"
-$appfiles += "https://sts3rus.blob.core.windows.net/artifacts/New%20Files/RunEngineOutputFiles.xml"
-$appfiles += "https://sts3rus.blob.core.windows.net/artifacts/New%20Files/S3RUSMasterRule.txt"
-$appfiles += "https://sts3rus.blob.core.windows.net/artifacts/New%20Files/S3RUSMasterRule.xml"
+$appfiles += "https://stneffwvd.blob.core.windows.net/artifacts/New%20Files/addparms.txt"
+$appfiles += "https://stneffwvd.blob.core.windows.net/artifacts/New%20Files/PairingCategories.txt"
+$appfiles += "https://stneffwvd.blob.core.windows.net/artifacts/New%20Files/RunEngineOutputFiles.xml"
+$appfiles += "https://stneffwvd.blob.core.windows.net/artifacts/New%20Files/neffwvdMasterRule.txt"
+$appfiles += "https://stneffwvd.blob.core.windows.net/artifacts/New%20Files/neffwvdMasterRule.xml"
 
 function Get-Installer {
     param (
@@ -45,7 +45,7 @@ foreach ($msi in $installers) {
 
 #fire off the non-msi installers
 start-process -FilePath "c:\installs\npp.7.9.Installer.exe" -ArgumentList '/S' -Verb runas
-start-process -FilePath "c:\installs\2020_09%20S3RUS%20System%20Update_mt.exe" -ArgumentList '-silent' -Verb runas
+start-process -FilePath "c:\installs\2020_09%20neffwvd%20System%20Update_mt.exe" -ArgumentList '-silent' -Verb runas
 start-process -FilePath "c:\installs\OneDriveSetup.exe" -ArgumentList '/allusers' -Verb runas
 start-process -FilePath "c:\installs\TableauReader-64bit-2020-3-1.exe" -ArgumentList '/quiet /norestart ACCEPTEULA=1' -Verb runas
 
@@ -56,7 +56,7 @@ function update-appfiles {
     )
 
     $filename =  split-path $url -leaf
-    $output = "C:\Program Files (x86)\S3RUS\S3RUS Crew Planning System\DataFiles\$filename"
+    $output = "C:\Program Files (x86)\neffwvd\neffwvd Crew Planning System\DataFiles\$filename"
     Invoke-WebRequest -Uri $url -OutFile $output
 }
 
@@ -65,8 +65,8 @@ foreach ($app in $appfiles){
 }
 
 #this file needs to overwrite the default file - not sure if this command does that.
-Invoke-WebRequest -Uri "https://sts3rus.blob.core.windows.net/artifacts/CrewPlanningUpdates/Users.txt" `
-    -OutFile "C:\Program Files (x86)\S3RUS\S3RUS Crew Planning System\Users\Users.txt"
+Invoke-WebRequest -Uri "https://stneffwvd.blob.core.windows.net/artifacts/CrewPlanningUpdates/Users.txt" `
+    -OutFile "C:\Program Files (x86)\neffwvd\neffwvd Crew Planning System\Users\Users.txt"
 
 #adds registry key allowing multiple sessions to session host.
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server"
